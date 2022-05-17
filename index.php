8126e5ec37eb64c292bb1ca77cccdea59f465908
William Hazevoets <williamhazevoets@gmail.com>
2022-05-17 13:41:19 +0200

    Fix Css stuff and put nav in their own div

--- index.php
+++ index.php
@@ -11,14 +11,19 @@
 <body>
    <div>
        <nav>
+           <div class="flex-home">
+             <a class= "home" href="">Home</a>
+           </div>
            <div class="flex-search">
             <p class="search">Search</p>
            </div>
-           <a class= "home" href="">Home</a>
-           <a class="login" href="">Log me in</a>
-           <a href="">Sign Up</a>
+           <div class="flex-login">
+             <a class="login" href="">Log me in</a>
+             <a class="signup" href="">Sign Up</a>
+           </div>
        </nav>
-       <img src="https://memegenerator.net/img/instances/52441577.jpg" alt="">
+       
    </div>
+   <img src="https://memegenerator.net/img/instances/52441577.jpg" alt="">
 </body>
 </html>
